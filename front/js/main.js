adf3e87fd9b9a0fd4ca32c4390cd463f9ef942a0
yurii <ignatovyw@gmail.com>
2024-05-16 14:08:13 +0300

    os detect script upd

--- front/js/main.js
+++ front/js/main.js
@@ -3,3 +3,33 @@ const darkBtn = document.querySelector('.dark-btn')
 darkBtn.addEventListener("click", () =>{
     document.body.classList.toggle("dark")
 })
+
+function getOS() {
+    const userAgent = window.navigator.userAgent;
+    const platform = window.navigator.platform;
+    const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
+    const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+    const iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+    let os = null;
+
+    if (macosPlatforms.includes(platform)) {
+        os = 'Mac OS';
+    } else if (iosPlatforms.includes(platform)) {
+        os = 'iOS';
+    } else if (windowsPlatforms.includes(platform)) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+    }
+
+    return os;
+}
+
+const operatingSystem = getOS();
+
+if (operatingSystem === 'Android') {
+    document.querySelector('.info__subtitle-top').style.backgroundPosition = "-5px, -7px"
+    document.querySelector('.guide__subtitle').style.backgroundPosition = "0, -.1em"
+}
